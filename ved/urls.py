e23b518ec8106341134fbd9d90d94fac89e94c62
dna <dharmesh.adhia@dowjones.com>
2015-04-15 21:50:18 -0400

    wip

--- ved/urls.py
+++ ved/urls.py
@@ -4,8 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^test/$', views.test, name='test'),
-    url(r'^t2/$', views.t2, name='test'),
-    url(r'^events/(?P<pyear>\d{4})/(?P<pmonth>\d{2})/$', views.events, name='events'),
-    url(r'^date/(?P<pmonth>\d{2})/(?P<pday>\d{2})/$', views.datedisp, name='events'),
+    url(r'^/events/(?P<pyear>\d{4})/(?P<pmonth>\d{2})/$', views.events, name='events'),
+    url(r'^/events/$', views.events, name='events'),
+    url(r'^/date/(?P<pmonth>\d{2})/(?P<pday>\d{2})/$', views.datedisp, name='events'),
 ]
